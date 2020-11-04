5f07aed65db801728dc9d4ce64c7901f3ac55781
Aditya Kamat <kamat.a@northeastern.edu>
2020-11-04 06:09:58 -0500

    war to jar

--- CodeDeploy/server_start.sh
+++ CodeDeploy/server_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo java -jar ~/target/webapp-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar ~/target/webapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 
 
