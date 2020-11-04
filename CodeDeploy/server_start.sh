4643d80a57ef05268b169f127b22155e8222fd7c
Aditya Kamat <kamat.a@northeastern.edu>
2020-11-04 02:27:57 -0500

    removed validate

--- CodeDeploy/server_start.sh
+++ CodeDeploy/server_start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo /usr/bin/java -jar  *.jar > /dev/null 2> /dev/null < /dev/null &
