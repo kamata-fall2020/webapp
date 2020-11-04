84483bf7aa92f38e7d5eea239bf548690238e1b9
Aditya Kamat <kamat.a@northeastern.edu>
2020-11-03 23:08:47 -0500

    Assignment6 (#31)
    * updated CD
    
    * updated CD
    
    * updated CD3
    
    * updated CD4
    
    * update cd flow
    
    * cd update
    
    * cd update2
    
    * cd update3
    
    * cd update4

--- CodeDeploy/beforeInstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# stop tomcat service
-echo "inside after stop"
-sudo systemctl stop tomcat
-sudo rm -rf /opt/tomcat/webapps/*
---
+++ CodeDeploy/server_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/server
+sudo /usr/bin/java -jar -Dserver.port=80 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
