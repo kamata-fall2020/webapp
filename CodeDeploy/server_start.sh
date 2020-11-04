63f903cb992c2acda0c20966d463be23bf56b17f
Aditya Kamat <kamat.a@northeastern.edu>
2020-11-04 05:37:48 -0500

    cd update 10

--- CodeDeploy/server_start.sh
+++ CodeDeploy/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo java -jar webapp-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar ~/target/webapp-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
 
