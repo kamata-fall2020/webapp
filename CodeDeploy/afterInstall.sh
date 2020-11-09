0c9eff3bd89e08725c1a4c79aae1d7de2d27e628
Aditya Kamat <kamat.a@northeastern.edu>
2020-11-09 01:33:15 -0500

    cloudwatch issue

--- CodeDeploy/afterInstall.sh
+++ CodeDeploy/afterInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo chmod +rwx /opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json
 
-sudo mv amazon-cloudwatch-agent-schema.json /opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json \
-    -s
+#sudo mv amazon-cloudwatch-agent-schema.json /opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json
+#
+#sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+#    -a fetch-config \
+#    -m ec2 \
+#    -c file:/opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json \
+#    -s
